436aab6ae3529dc39c096c10f123196651ac94f3
Frizlab <fload@me.com>
2019-03-31 21:45:47 +0200

    Convert project to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,29 +1,24 @@
-// swift-tools-version:4.0
-
+// swift-tools-version:5.0
 import PackageDescription
-
 
 
 let package = Package(
 	name: "CollectionLoader",
+	platforms: [
+		.macOS(.v10_10),
+		.iOS(.v8),
+		.tvOS(.v9),
+		.watchOS(.v2)
+	],
 	products: [
-		.library(
-			name: "CollectionLoader",
-			targets: ["CollectionLoader"]
-		)
+		.library(name: "CollectionLoader", targets: ["CollectionLoader"]),
 	],
 	dependencies: [
-		.package(url: "git@github.com:happn-tech/KVObserver.git", from: "0.9.4"),
-		.package(url: "git@github.com:happn-tech/AsyncOperationResult.git", from: "1.0.5")
+		.package(url: "https://github.com/happn-tech/KVObserver.git", from: "0.9.4"),
+		.package(url: "https://github.com/happn-tech/AsyncOperationResult.git", from: "1.0.5")
 	],
 	targets: [
-		.target(
-			name: "CollectionLoader",
-			dependencies: ["AsyncOperationResult", "KVObserver"]
-		),
-		.testTarget(
-			name: "CollectionLoaderTests",
-			dependencies: ["CollectionLoader"]
-		)
+		.target(name: "CollectionLoader", dependencies: ["AsyncOperationResult", "KVObserver"]),
+		.testTarget(name: "CollectionLoaderTests", dependencies: ["CollectionLoader"])
 	]
 )
