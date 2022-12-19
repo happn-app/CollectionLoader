b0d1cc70f5cf6b1cd08b176579664172ee47a950
Frizlab <fload@me.com>
2022-12-20 00:22:22 +0100

    Make Package.swift more readable

--- Package.swift
+++ Package.swift
@@ -1,15 +1,25 @@
 // swift-tools-version:5.1
 import PackageDescription
+func buildArray<Element>(of type: Any.Type = Element.self, _ builder: (_ collection: inout [Element]) -> Void) -> [Element] {
+	var ret = [Element](); builder(&ret); return ret
+}
+
 
 
 let package = Package(
 	name: "CollectionLoader",
+	
+	
 	products: buildArray{
 		$0.append(.library(name: "CollectionLoader", targets: ["CollectionLoader"]))
 	},
+	
+	
 	dependencies: buildArray{
 		$0.append(.package(url: "https://github.com/happn-app/KVObserver.git", from: "0.9.5"))
 	},
+	
+	
 	targets: buildArray{
 		$0.append(.target(name: "CollectionLoader", dependencies: buildArray{
 			$0.append(.product(name: "KVObserver", package: "KVObserver"))
@@ -17,10 +27,3 @@ let package = Package(
 		$0.append(.testTarget(name: "CollectionLoaderTests", dependencies: ["CollectionLoader"]))
 	}
 )
-
-
-func buildArray<Element>(of type: Any.Type = Element.self, _ builder: (_ collection: inout [Element]) -> Void) -> [Element] {
-	var ret = [Element]()
-	builder(&ret)
-	return ret
-}
