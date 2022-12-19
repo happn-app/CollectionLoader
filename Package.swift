7507ba815604bd269c96371aaeae8ee95957ecbb
Frizlab <fload@me.com>
2022-12-19 17:13:55 +0100

    Use new format for Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,23 @@ import PackageDescription
 
 let package = Package(
 	name: "CollectionLoader",
-	products: [
-		.library(name: "CollectionLoader", targets: ["CollectionLoader"]),
-	],
-	dependencies: [
-		.package(url: "https://github.com/happn-app/KVObserver.git", from: "0.9.5")
-	],
-	targets: [
-		.target(name: "CollectionLoader", dependencies: ["KVObserver"]),
-		.testTarget(name: "CollectionLoaderTests", dependencies: ["CollectionLoader"])
-	]
+	products: buildArray{
+		$0.append(.library(name: "CollectionLoader", targets: ["CollectionLoader"]))
+	},
+	dependencies: buildArray{
+		$0.append(.package(url: "https://github.com/happn-app/KVObserver.git", from: "0.9.5"))
+	},
+	targets: buildArray{
+		$0.append(.target(name: "CollectionLoader", dependencies: buildArray{
+			$0.append(.product(name: "KVObserver", package: "KVObserver"))
+		}))
+		$0.append(.testTarget(name: "CollectionLoaderTests", dependencies: ["CollectionLoader"]))
+	}
 )
+
+
+func buildArray<Element>(of type: Any.Type = Element.self, _ builder: (_ collection: inout [Element]) -> Void) -> [Element] {
+	var ret = [Element]()
+	builder(&ret)
+	return ret
+}
